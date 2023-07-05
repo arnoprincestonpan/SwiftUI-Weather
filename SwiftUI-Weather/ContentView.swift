d272b09303cbe959ffe3e9b3644c0a380f7ecb4b
ARNO PAN <apan11@my.bcit.ca>
2023-07-05 16:22:00 -0700

    one file everythintg is in their own view

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -13,36 +13,16 @@ struct ContentView: View {
             BackgroundView(topColor: .blue, bottomColor: Color("babyBlue"))
             VStack{
                 CityTextView(cityName: "Cupertino", stateName: "CA")
-                VStack{
-                    Image(systemName: "cloud.sun.fill")
-                        .renderingMode(.original)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 184, height: 184)
-                    Text("76°")
-                        .font(.system(size: 68, weight: .medium))
-                        .foregroundColor(.white)
-                }
-                .padding(.bottom, 40)
                 
-                HStack(spacing: 24){
-
-                    WeatherDayView(dayOfWeek: "TUES", imageName: "sun.max.fill", temperature: 77)
-                    WeatherDayView(dayOfWeek: "WED", imageName: "cloud.sun.fill", temperature: 70)
-                    WeatherDayView(dayOfWeek: "THURS", imageName: "cloud.heavyrain.fill", temperature: 68)
-                    WeatherDayView(dayOfWeek: "FRI", imageName: "cloud.rain.fill", temperature: 65)
-                    WeatherDayView(dayOfWeek: "SAT", imageName: "cloud.rain.fill", temperature: 64)
-                }
+                MainWeatherStatusView(imageName: "cloud.sun.fill", temperature: 76)
+                
+                WeekWeatherStatusView()
                 Spacer()
                 
                 Button{
                     print("tap")
                 } label: {
-                    Text("Change Day Time")
-                        .frame(width: 314, height: 64)
-                        .background(Color.white)
-                        .font(.system(size: 24, weight: .bold, design: .default))
-                        .cornerRadius(16.0)
+                    WeatherButtonLabel(title: "Change Day Time", textColor: .blue, backgroundColor: .white)
                 }
                 
                 Spacer()
@@ -103,5 +83,54 @@ struct CityTextView: View {
             .font(.system(size: 32, weight: .medium, design: .default))
             .foregroundColor(.white)
             .padding()
+    }
+}
+
+struct MainWeatherStatusView: View {
+    // variables that change
+    
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View{
+        VStack{
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 184, height: 184)
+            Text("\(temperature)°")
+                .font(.system(size: 68, weight: .medium))
+                .foregroundColor(.white)
+        }
+        .padding(.bottom, 40)
+    }
+}
+
+struct WeekWeatherStatusView: View {
+    var body: some View {
+        HStack(spacing: 24){
+            
+            WeatherDayView(dayOfWeek: "TUES", imageName: "sun.max.fill", temperature: 77)
+            WeatherDayView(dayOfWeek: "WED", imageName: "cloud.sun.fill", temperature: 70)
+            WeatherDayView(dayOfWeek: "THURS", imageName: "cloud.heavyrain.fill", temperature: 68)
+            WeatherDayView(dayOfWeek: "FRI", imageName: "cloud.rain.fill", temperature: 65)
+            WeatherDayView(dayOfWeek: "SAT", imageName: "cloud.rain.fill", temperature: 64)
+        }
+    }
+}
+
+struct WeatherButtonLabel: View {
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    
+    var body: some View {
+        Text(title)
+            .frame(width: 314, height: 64)
+            .background(backgroundColor)
+            .foregroundColor(textColor)
+            .font(.system(size: 24, weight: .bold, design: .default))
+            .cornerRadius(16.0)
     }
 }
